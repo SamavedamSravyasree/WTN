3204e19e0cbc71f2648d970f7135be600ee87f1d
DELL <DELL@192.168.0.7>
2020-06-11 19:55:23 +0530

    This is exercise-1 of language basics

---
+++ Milestone1/src/com/wipro/language/basics/ex1.java
@@ -0,0 +1,9 @@
+package com.wipro.language.basics;
+
+public class ex1 {
+	public static void main(String[] args) {
+		String s=args[0];
+		String s1=args[1];
+		System.out.print(s +" Technologies "+ s1);
+	}
+}
