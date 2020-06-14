f24d02621a03de57876c041fdd76abf1306195e3
Sravya <samavedamsravya@gmail.com>
2020-06-14 22:44:07 +0530

    These are exercises-6 of flowcontrol

--- Milestone1/src/com/wipro/flow/control/Percentageofinterest.java
+++ Milestone1/src/com/wipro/flow/control/Percentageofinterest.java
@@ -4,17 +4,17 @@ public class Percentageofinterest {
 public static void main(String[] args) {
 	String s=args[0];
 	int a=Integer.parseInt(args[1]);
-	if(s=="Female" && (a>=1 && a<=58) ) {
+	if(s.equals("Female") && (a>=1 && a<=58) ) {
 		System.out.println("Percentage of interest = 8.2%");
 	}
-	if(s=="Female" && (a>=59 && a<=100)) {
+	if(s.equals("Female") && (a>=59 && a<=100)) {
 		System.out.println("Percentage of interest = 9.2%");
 	}
-    if(s=="Male" && (a>=1 && a<=58)) 
+    if(s.equals("Male") && (a>=1 && a<=58)) 
     {
 	System.out.println("Percentage of interest = 8.4%");
 }
-      if(s=="Male" && (a>=59 && a<=100))
+      if(s.equals("Male") && (a>=59 && a<=100))
       {
 	System.out.println("Percentage of interest = 10.5%");
     }
