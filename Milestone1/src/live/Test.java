40511afce3d1c56fad9c7c4b57aa87eea58c2316
DELL <DELL@192.168.0.5>
2020-07-06 07:21:12 +0530

    This is exercise of interface.

---
+++ Milestone1/src/live/Test.java
@@ -0,0 +1,15 @@
+package live;
+
+
+import music.string.*;
+import music.wind.*;
+public class Test  {
+public static void main(String[] args) {
+	Veena v=new Veena();
+	Saxophone s=new Saxophone();
+	v.play();
+	s.play();
+	
+}
+
+}
